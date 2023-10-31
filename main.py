19fd2125f12fcb1e6106d06780b6397c50048928
BadGuy098 <35214602+BadGuy098@users.noreply.github.com>
2023-10-31 15:26:45 -0400

    2nd Edition with Front Branch, Back Branch and Skip Branch

--- main.py
+++ main.py
@@ -8,20 +8,16 @@ def largest_number_of_vehicles(data):
     def branch_and_bound(i, weight_sum, v_count):
         if (i, weight_sum, v_count) in memo:
             return memo[(i, weight_sum, v_count)]
-
         if i == n:
             return v_count
-
         if weight_sum + w[i] <= max_w:
             front_branch = branch_and_bound(i + 1, weight_sum + w[i], v_count + 1)
             back_branch = branch_and_bound(i + 1, weight_sum, v_count)
             output = max(front_branch, back_branch)
         else:
             output = branch_and_bound(i + 1, weight_sum, v_count)
-
         memo[(i, weight_sum, v_count)] = output
         return output
-
     return branch_and_bound(0, 0, 0)
 
 
